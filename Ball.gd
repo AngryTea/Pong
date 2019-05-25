9a91912386e1fa67a00e42ea0634c252f9111bc6
enrico-kaack <e.kaack@live.de>
2019-05-25 18:19:35 +0200

    Added: movement of ball

---
+++ Ball.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export var speed: int = 10
+var direction = Vector2(0,0)
+func start():
+	direction = Vector2((randi() % 3 ) - 1, 0)
+	
+func _ready():
+	start()
+	
+func _process(delta):
+	var collision = move_and_collide(direction * speed)
+	if collision:
+		direction = direction.bounce(collision.normal)
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
