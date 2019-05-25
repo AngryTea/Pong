fe40d2b24c22eeee861fb49a14ee76d87c3c0fe2
enrico-kaack <e.kaack@live.de>
2019-05-25 19:48:53 +0200

    Final

--- Ball.gd
+++ Ball.gd
@@ -10,18 +10,14 @@ func start():
 	else:
 		direction.x = 1
 	
-func _ready():
-	start()
-	
 func _process(delta):
 	rotate(0.1)
-	
 	var collision = move_and_collide(direction * speed)
 	if collision:
 		direction = direction.bounce(collision.normal)
 		direction.y = randf() * 2 - 1
 		direction = direction.normalized()
-		print(collision)
+
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
 			queue_free()
--- Main.gd
+++ Main.gd
@@ -2,8 +2,22 @@ extends Node2D
 
 const Ball = preload("res://Ball.tscn")
 
+var ball;
+
+var inPause = true
 
 func ballReset():
-	var ball = Ball.instance()
+	ball = Ball.instance()
 	ball.position = OS.get_real_window_size() / 2
+	ball.name = "Ball"
 	add_child(ball)
+	inPause = true
+
+func _ready():
+	ballReset()
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		inPause = false
+		ball.start()
+		
