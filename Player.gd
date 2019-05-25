4cca00c5fbc3ec351d14c458c98f18a594adb629
dp-dev <development@studware.de>
2019-05-25 18:56:41 +0200

    Add movement and labels

--- Player.gd
+++ Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
-export var speed: int = 100
+export var speed: int = 10
 
 func _process(delta):
 	if Input.is_action_pressed(name + "_down"):
+		move_and_collide(Vector2(0,1) * speed)
+	if Input.is_action_pressed(name + "_up"):
 		move_and_collide(Vector2(0,-1) * speed)
