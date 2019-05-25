8de9b265f7e7491fe3962c9c153dc496bb26f7e5
enrico-kaack <e.kaack@live.de>
2019-05-25 18:43:16 +0200

    Added bouncing top and bottom

--- Ball.gd
+++ Ball.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export var speed: int = 10
 var direction = Vector2(0,0)
+
+
 func start():
 	direction = Vector2((randi() % 3 ) - 1, 0)
 	
@@ -9,10 +11,13 @@ func _ready():
 	start()
 	
 func _process(delta):
+	rotate(0.1)
+	
 	var collision = move_and_collide(direction * speed)
 	if collision:
 		direction = direction.bounce(collision.normal)
 		direction.y = randf() * 2 - 1
 		direction = direction.normalized()
+		print(collision)
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
